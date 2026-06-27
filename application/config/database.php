a4fa68be7aaeda9ad186584031f20c322c3160cb
Faiq-DataSci <athafaiq313@gmail.com>
2026-06-27 15:26:47 +0700

    Memodifikasi config

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '127.0.0.1',
+	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => '',
-	'database' => 'db_pariwisata',
+	'database' => 'website_pariwisata',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
